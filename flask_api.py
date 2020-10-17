4781168dc85a5bff774068fe3494481395a8e0cd
SAB-6 <shereef.bankole@yahoo.com>
2020-10-17 23:47:39 +0100

    model updated -class- and saved

---
+++ flask_api.py
@@ -0,0 +1,141 @@
+# Import required libraries
+import joblib
+from flask import Flask, request
+import flasgger
+from flasgger import Swagger
+
+app = Flask(__name__)
+Swagger(app)
+
+# load preprocessor pipeline and model
+
+model = joblib.load('./model/model.pkl')
+
+
+# web page that handles user's query and displays model results
+@app.route('/')
+def home():
+    return "A web app for predicting hepatitis"
+
+
+@app.route('/predict', methods=['Get'])
+def predict():
+    """Predicting hepatitis
+    Specifies the features to make prediction under the parameters.
+    ---
+    parameters: 
+      - name: age
+        in: query
+        type: number
+        required: true
+      - name: sex
+        in: query
+        type: number
+        required: true
+      - name: steroid
+        in: query
+        type: number
+        required: true
+      - name: antivirals
+        in: query
+        type: number
+        required: true
+        - name: fatigue'
+        in: query
+        type: number
+        required: true
+        - name: malaise
+        in: query
+        type: number
+        required: true
+        - name: anorexia
+        in: query
+        type: number
+        required: true
+        - name: liver_big
+        in: query
+        type: number
+        required: true
+        - name: liver_firm'
+        in: query
+        type: number
+        required: true
+        - name: spleen_palpable
+        in: query
+        type: number
+        required: true
+        - name: spiders
+        in: query
+        type: number
+        required: true
+        - name: ascites
+        in: query
+        type: number
+        required: true
+        - name: varices
+        in: query
+        type: number
+        required: true
+        - name: bilirubin
+        in: query
+        type: number
+        required: true
+        - name: alk_phosphate
+        in: query
+        type: number
+        required: true
+        - name: sgot
+        in: query
+        type: number
+        required: true
+        - name: albumin
+        in: query
+        type: number
+        required: true
+        - name: protime
+        in: query
+        type: number
+        required: true
+        - name: histology
+        in: query
+        type: number
+        required: true
+    responses:
+        200:
+            description: The output values 
+    """
+    age = request.args.get("age")
+    sex = request.args.get("sex")
+    steroid = request.args.get("steroid")
+    antivirals = request.args.get("antivirals")
+    fatigue =request.args.get("fatigue")
+    malaise = request.args.get("malaise")
+    anorexia = request.args.get("anorexia")
+    liver_big = request.args.get("liver_big")
+    liver_firm=request.args.get("liver_firm")
+    spleen_palpable = request.args.get("spleen_palpable")
+    spiders=request.args.get("spiders")
+    ascites = request.args.get("ascites")
+    varices=request.args.get("varices")
+    bilirubin = request.args.get("bilirubin")
+    alk_phosphate =request.args.get("alk_phosphate")
+    sgot = request.args.get("sgot")
+    albumin = request.args.get("albumin")
+    protime =request.args.get("protime")
+    histology = request.args.get("histology")
+    
+    prediction= model.predict([[age, sex, steroid, antivirals, 
+    fatigue, malaise, anorexia, liver_big, liver_firm, spleen_palpable,
+    spiders, ascites, varices, bilirubin, alk_phosphate, sgot, albumin,
+    protime, histology]])
+
+    print(prediction[0])
+    return "Hello The answer is"+str(prediction[0])                     
+
+
+def main():
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+
+if __name__ == '__main__':
+    main()
--- model/model.py
+++ model/model.py
@@ -11,14 +11,14 @@ import joblib
 
 
 class model:
-    def__init__(self)
-    self.df = None
+    def __init__(self):
+        self.df = None
 
-    def read_data(self, file_path):
+    def read_data(self, file_path, columns):
         print("\nReading dataframe\n")
-        self.df = pd.read_table(file_path, sep=",")
+        self.df = pd.read_table(file_path, sep=",", names = columns)
 
-    def preproces(self, float_cols):
+    def preprocess(self, float_cols):
         """
         Preprocess dataframe
         Args:
@@ -27,38 +27,49 @@ class model:
         print("preprocessing data")
         # replace missing values (?) with zeros
         self.df = self.df.replace("?", 0)
-        self.df.columns = self.df.columns.str.lower().str.replace('" ", "_")
+        self.df.columns = self.df.columns.str.lower().str.replace(" ", "_")
         int_columns = [col for col in list(
             self.df.columns) if self.df[col].dtypes != int if col not in float_cols]
+        # print(f"columns: {self.df.columns}")
+        # print(self.df.isnull().sum().sort_values(ascending=False))
         self.df[int_columns] = self.df[int_columns].astype(int)
-        print(self.df.head)
+        # print(self.df.head)
         print("\n\n")
         # change both bilirubin and albumin columns to float
         self.df[float_cols] = self.df[float_cols].astype(float)
         self.df[target] = self.df[target].map({1: 1, 2: 0})
-        print(self.df.dtypes)
+        # print(self.df.dtypes)
+        df = self.df
+        return df
 
     @staticmethod
-    def train_test_split(features, target):
-        X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.3,
-                                                            stratify=target, random_state=42)
+    def train_test_split(X, y):
+        """ Split data into 
+        training and test sets
+        X = type - dataframe or array- features
+        target = type -dataframe or array- target
+        df = preprocessed dataframe
+        """
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,
+                                                            stratify=y, random_state=42)
         return X_train, X_test, y_train, y_test
 
-
-def get_features(self, target, df):
-    """
-    Get feature columns
-    Args:
-        df - preprocessed dataframe
-        target - target column, type- string
-    Return:
-        feature columns
-    """
-    features = [col for col in df.columns if col not in target]
-    return features
+    @staticmethod
+    def get_features(target, df):
+        """
+        Get feature columns
+        Args:
+            df - preprocessed dataframe
+            target - target column, type- string
+        Return:
+            feature columns
+        """
+        features = [col for col in df.columns if col not in target]
+        print(f"feature: {features}")
+        return features
 
     @staticmethod
-    def pipe_model(X_train, X_test, y_train, y_test):
+    def pipe_model(X_train, y_train):
         # instantiate scaler
         scaler = StandardScaler()
         # each of thesesticRegression(solver='liblinear', random_state= 42)
@@ -70,7 +81,7 @@ def get_features(self, target, df):
         pipe = Pipeline([('scaler', scaler), ('classifier', clf1)])
         # create the parameter dictionary for clf1
         params1 = {}
-        params1["classifier__penalty"] = ["l1", "l2", "none"]
+        params1["classifier__penalty"] = ["l1", "l2"]
         params1["classifier__C"] = [0.1, 1, 10]
         params1["classifier"] = [clf1]
         # create the parameter dictionary for clf2
@@ -80,28 +91,45 @@ def get_features(self, target, df):
         params2["classifier"] = [clf2]
         # create the parameter dictionary for clf2
         params3 = params2.copy()
-        param3["classifier"] = [clf3]
+        params3["classifier"] = [clf3]
+
         params = [params1, params2, params3]
         # Use GridSearchCV to get the best model
         grid = GridSearchCV(pipe, params, cv=5)
         grid.fit(X_train, y_train)
-        best_param = grid.best_params_
-        print("\nbest_param")
+        # best_param = grid.best_params_
         best_estimator = grid.best_estimator_
+        print(f"\nbest_estimator: {best_estimator}")
         return best_estimator
 
-        @staticmethod
-        def save_model(X_train, X_test, y_train, y_test, model_path):
-            best_estimator.fit(X_train, y_train)
-            # Save model to directory
-            print("Saving model to {}".format(model_path))
-            with open(model_path, 'wb') as file:
-                joblib.dump(model, file)
-            print("\nmodel saved\n")
-
+    @staticmethod
+    def save_model(X_train, y_train, model_path, best_estimator):
+        model = best_estimator
+        best_estimator.fit(X_train, y_train)
+        # Save model to directory
+        print("Saving model to {}".format(model_path))
+        with open(model_path, 'wb') as file:
+            joblib.dump(model, file)
+        print("\nmodel saved\n")
+        
 
 if __name__ == "__main__":
+    columns = ["Class", "AGE", "SEX", "STEROID", "ANTIVIRALS", "FATIGUE", "MALAISE", "ANOREXIA", "LIVER BIG", "LIVER FIRM",
+                "SPLEEN PALPABLE", "SPIDERS", "ASCITES", "VARICES", "BILIRUBIN", "ALK PHOSPHATE", "SGOT", "ALBUMIN",
+                "PROTIME", "HISTOLOGY"]
     target = "class"
-    file_path = "../hepatitis.py"
+    file_path = "../hepatitis.data"
     float_cols = ["bilirubin", "albumin"]
-    model_path = "./"
+    model_path = "./model.pkl"
+    data = model()
+    data.read_data(file_path, columns)
+    df = data.preprocess(float_cols)
+    features = data.get_features(target, df)
+    X = df[features]
+    y = df[target]
+    
+    X_train, X_test, y_train, y_test = data.train_test_split(X,y)
+    best_estimator = data.pipe_model(X_train, y_train)
+    data.save_model(X_train, y_train, model_path, best_estimator)
+
+
